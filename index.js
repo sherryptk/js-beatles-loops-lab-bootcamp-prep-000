b53164813ce28de2d72dda7dbd88d1fac59e8fee
Sherry <sherryptk@gmail.com>
2017-09-20 17:35:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function theBeatlesPlay(music, instruments){
+  var array[]
+
+  for (var i = 0; i < array.length; i++) {
+    array[i].push(music[i]) + "plays " + instruments[i])
+  }
+  return array
+}
+
+// The first time through the loop, the body of the loop should 
+//create a string using the first index of the musicians array 
+// and the first index of the instruments array: "John Lennon plays guitar". 
+// This string should be added to the empty array you created. The loop should make the same sentence for every member 
+// of the musicians array. The function should return the array of new strings.
