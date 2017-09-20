dc49cd3ee5845dd2d72154e176635c4242dc2f5a
Sherry <sherryptk@gmail.com>
2017-09-20 18:03:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,15 @@ function theBeatlesPlay(music, instruments){
   return array
 }
 
-// The first time through the loop, the body of the loop should
-//create a string using the first index of the musicians array
-// and the first index of the instruments array: "John Lennon plays guitar".
-// This string should be added to the empty array you created. The loop should make the same sentence for every member
-// of the musicians array. The function should return the array of new strings.
+function johnLennonFacts(array){
+  var i=array.length
+  while (i>0) {
+    array.push("!!!")
+    i-=
+  }
+
+  return array
+}
+
+// Use a while loop to loop over the facts array and add "!!!" to the end of every fact. 
+// The function should return an array of strings with exclamation points.
